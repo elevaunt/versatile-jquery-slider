f58ce73c12a43d1ea45bfdb1ee3468d6b847d17d
Lee Porter <lee.porter@eyeqinsights.com>
2016-08-11 22:01:43 -0500

    Initial commit

---
+++ readme.txt
@@ -0,0 +1,123 @@
+=== Versitile jQuery Slider ===
+Contributors: elevaunt
+Tags: slider, slideshow, jquery slider, jquery slideshow, responsive slider, responsive slideshow	
+Author URI: http://www.elevaunt.com
+Plugin URI: http://www.elevaunt.com/plugins/versatile-jquery-slider/
+Requires at least: 3.0.1
+Tested up to: 4.5.3
+Stable tag: 1.0
+License: GPLv3
+License URI: http://www.gnu.org/licenses/gpl-3.0.txt
+
+Set up an easy, versatile, responsive slider with images or any HTML content.  Powered by jQuery Cycle2.
+
+== Description ==
+
+The Versatile jQuery Slider helps you set up an easy, versatile, responsive slider with images or any HTML content.  Powered by [jQuery Cycle2](http://jquery.malsup.com/cycle2).
+
+I’ve often looked for a good plugin that would allow me to quickly and easily add the jQuery Cycle2 plugin to a site, but I couldn’t find any good ones… so I built this.  Basically, the Versatile jQuery Slider (VJS Slider) is a wrapper that pulls in most of the available options into a shortcode for easy use. So you’ll want to check out the [jQuery Cycle2 options](http://jquery.malsup.com/cycle2/api) to know what is available.
+
+The nice part is, the jQuery Cycle2 scripts are only loaded on the pages that they are used on, and only the necessary scripts are loaded.  So if you need the carousel plugin, it will automatically be added in if the `fx` attribute is set `carousel`.  You need to center vertically?  Set the `center-vert` attribute to `true`.
+
+Easy as that!
+
+
+= Demos =
+Check out the [demos page](http://elevaunt.com/plugins/versatile-jquery-slider/demos).
+
+= Parameters =
+VJS Slider has some unique paramaters that jQuery Cycle2 doesn't have.  Also there are a some changes to a few of the jQuery Cycle2 parameters.  [Check them out](http://elevaunt.com/plugins/versatile-jquery-slider).
+
+
+== Installation ==
+
+1. Install the plugin through the WordPress plugins screen directly by searching for Versatile jQuery Slider, or upload the plugin files to the `/wp-content/plugins/versatile-jquery-slider` directory
+1. Activate the plugin through the 'Plugins' screen in WordPress
+
+
+= Usage = 
+To use the VJS Slider, you'll use the shortcode `[[vjs_slider] your slide code here [/vjs_slider]]`, then you're all set for the basic, default usage.
+
+If you want to get fancy, follow along with the [jQuery Cycle2 demos](http://jquery.malsup.com/cycle2/demo), and wherever `data-cycle-attribute` is used, drop `data-cycle-` and all the rest is the same (in most cases).
+
+I'm not going to list out all the options here because they are all very well documented on the [jQuery Cycle2 site](http://jquery.malsup.com/cycle2).  Check it all out over there, then make some cool stuff with the VJS Slider!
+
+
+= Important Differences =
+There are a couple important changes that had to be made because they wouldn't work with the shortcode.  The following attributes are affected:
+
+* **slides**
+If you use the `slide` attribute, don't add `>`, it will automatically be assumed.  If you use ' If you use `>` like the demos on the jQuery Cycle2 site show, then it will mess up the shortcode and your slider won't work.
+
+* **next** and **prev**
+If you want to set your own selectors for next and prev, you'll need to add your own CSS.
+***Note:*** if the navigation elements are inside the slider container they will be hidden by default. (see `navs `attribute below).
+
+* **slide-css**
+This attribute is not used because it doesn't work reliably inside the shortcode. If you want slide specific CSS, just add your own class to your slide and add your own CSS.
+
+* **swipe**
+Swipe is turned on and the necessary js added automatically because let's face it, who doesn't want that?
+
+= VJS Slider Specific Attributes =
+VJS Slider has a few attribute unique to the plugin that are designed to help you out.
+
+ * **id**
+By default, the VJS Slider will have `id="vjs-slider"`. If you want to change it, use the `id` attribute. 
+***Note:*** if you have more than one slider on the same page, make sure they all have a different id.
+
+ * **class**
+Give your slider a class and style to your heart's content.  By default all sliders will have the class `vjs-slider`. If you set one here, it will be in addition to that class.
+
+ * **css**
+VJS Slider comes with some very basic CSS.  If you don't want to use it, set the `css` attribute to `false`. Then you can use your own without having to override anything.
+
+ * **ie-fade**
+Set to `true` to include the fade/fadeout plugin for old versions of IE. This plugin corrects issues that arise when cleartype is used with opacity. See the [jQuery Cycle2 docs](http://jquery.malsup.com/cycle2/download/).
+ * **navs**
+This will enable next and prev navigation links. Set to `inside` for the links to live inside the slider container. Set to `outside` for the links to live outside the slider container. 
+***Note:*** If set to `outside`, you'll need to add your own CSS to make things look right.
+
+ * **nav-next**
+When **navs** are enabled, the default will create a right arrow link. Use this attribute to change it to something else. It will accept plain text, `img` tag, or any html.
+
+ * **nav-prev**
+When **navs** are enabled, the default will create a left arrow link. Use this attribute to change it to something else. It will accept plain text, `img` tag, or any html.
+
+ * **nav-selector**
+Give the navigation wrapper a class or id so that you can style as you wish.
+
+ * **theme-fix**
+Sometimes a theme includes a version of jQuery Cycle (usually it's an older version). When this happens, it will create a conflict with VJS Slider. Set this attribute to `true` to fix the conflict.
+
+ * **width**
+By default, the slider container will expand the full width of it's parent container. You can set your own width with this attribute .
+
+
+== Frequently Asked Questions ==
+
+= Where can I find detailed instructions =
+
+You can view the docs at the [Versatile jQuery Slider plugin homepage](http://elevaunt.com/plugins/versatile-jquery-slider).  There you'll find an overview of how to use the shortcode.  For details regarding all the available parameters, please see the [jQuery Cycle2 plugin options page](http://jquery.malsup.com/cycle2/api).
+
+= My HTML slides aren't working =
+
+Make sure you've included the `slides` parameter and correctly set it's value to the element of each slide (e.g. `div` or `span`).  Also make sure you did not include `>`, the VJS Slider will take care of that part for you.
+
+= The slider looks like it works, but then it just breaks =
+
+First double checked that you don't have any typos (missing quotes, etc).
+
+Next, make sure that you've structured everything inside the shortcode correctly (see the [VJS Slider demos](http://elevaunt.com/plugins/versatile-jquery-slider) and the [jQuery Cycle2 demos](http://jquery.malsup.com/cycle2/demo)).
+
+If you're still having problems, there's a chance that your theme includes a version of jQuery Cycle2.  Try adding `theme-fix="true"` as a parameter and that should do the trick.
+
+== Screenshots ==
+
+
+== Changelog ==
+
+= 1.0 (2016-08-11): =
+* Initial Release
+
+== Upgrade Notice ==
---
+++ versatile-jquery-slider.php
@@ -0,0 +1,343 @@
+<?php
+/*
+Plugin Name: Versatile jQuery Slider
+Description: Set up an easy, versatile, responsive slider with images or any HTML content.  Powered by jQuery Cycle2.
+Version: 1.0
+Author: Lee Porter
+Plugin URI:  http://www.elevaunt.com/plugins/versatile-jquery-slider
+Author URI:  http://www.elevaunt.com
+*/
+
+/*
+ 
+Shortcode for the jQuery Cycle2 plugin by malsup <a href="http://jquery.malsup.com/cycle2/">http://jquery.malsup.com/cycle2/</a>
+
+License:     GPL2
+
+Versatile jQuery Slider is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+ 
+Versatile jQuery Slider is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License
+along with Versatile jQuery Slider. If not, see {License URI}.
+
+*/
+
+
+function vjs_slider( $atts, $content = null ) {
+
+  $vjs_settings = shortcode_atts( array(
+    'class'         => '',
+    'css'           => 'true',
+    'id'            => 'vjs-slider',
+    'ie-fade'        => '',
+    'navs'          => '',
+    'nav-next'      => '<img src="' . plugins_url( '/img/arrow-right.png', __FILE__ ) . '" >',
+    'nav-prev'      => '<img src="' . plugins_url( '/img/arrow-left.png', __FILE__ ) . '" >',
+    'nav-selector'  => '',
+    'theme-fix'     => '',
+    'width'         => '',
+  ), $atts );
+
+  $vjs_opts = shortcode_atts(array(
+    // standard options
+    'allow-wrap'          => '',  // Default is true
+    'auto-height'         => '',  // Default is 0  // setting this option to false disables autoHeight logic
+    'auto-selector'       => '',  // Default is '.cycle-slideshow[data-cycle-auto-init!=false]'
+    'caption'             => '',  // Default is '> .cycle-caption'
+    'caption-template'    => '',  // Default is '{{slideNum}} / {{slideCount}}'
+    'delay'               => '',  // Default is 0
+    'disabled-class'      => '',  // Default is 'disabled'
+    'easing'              => '',  // Default is null
+    'fx'                  => '',  // Default is 'fade'
+    'hide-non-active'     => '',  // Default is true
+    'loader'              => '',  // Default is false
+    'loop'                => '',  // Default is 0
+    'manual-fx'           => '',  // Default is undefined
+    'manual-speed'        => '',  // Default is undefined
+    'manual-trump'        => '',  // Default is true
+    'next'                => '',  // Default is '> .cycle-next'
+    'next-event'          => '',  // Default is 'click.cycle'
+    'overlay'             => '',  // Default is '> .cycle-overlay'
+    'overlay-template'    => '',  // Default is '<div>{{title}}</div><div>{{desc}}</div>'
+    'pager'               => '',  // Default is > .cycle-pager
+    'pager-active-class'  => '',  // Default is cycle-pager-active
+    'pager-event'         => '',  // Default is click.cycle
+    'pager-event-bubble'  => '',  // Default is undefined
+    'pager-template'      => '',  // Default is '<span>&bull;</span>'
+    'pause-on-hover'      => '',  // Default is false
+    'prev'                => '',  // Default is '> .cycle-prev'
+    'prev-event'          => '',  // Default is 'click.cycle'
+    'progressive'         => '',  // Default is false
+    'random'              => '',  // Default is false
+    'reverse'             => '',  // Default is false
+    'slide-active-class'  => '',  // Default is 'cycle-slide-active'
+    'slide-class'         => '',  // Default is cycle-slide
+    // 'slide-css'           => '',  // Default is { position: 'absolute', top: 0, left: 0 } -- Used on the slide element 
+    'slides'              => '',  // Default is '> img'
+    'speed'               => '',  // Default is 500
+    'starting-slide'      => '',  // Default is 0
+    'sync'                => '',  // Default is true
+    'timeout'             => '',  // Default is 4000
+    'tmpl-regex'          => '',  // Default is '{{((.)?.*?)}}'
+    'update-view'         => '',  // Default is 0
+
+    // carousel plugin
+    // REQUIRES CAROUSEL PLUGIN
+    'carousel-fluid'      => '',  // Default is false
+    'carousel-vertical'   => '',  // Default is false
+    'carousel-visible'    => '',  // Default is as many as will fit  // Number of slides visible
+
+    // center slides plugin
+    // REQUIRES CENTER PLUGIN
+    'center-horz'         => '',  // Default is false
+    'center-vert'         => '',  // Default is false
+
+    // REQUIRES SWIPE PLUGIN IF NOT USING JQUERY MOBILE
+    'swipe'               => 'true',  // Default is false
+
+    // youtube plugin
+    // REQUIRES YOUTUBE PLIGIN 
+    'youtube'                   => '',  // Default is false
+    'youtube-autostart'         => '',  // Default is false
+    'youtube-autostop'          => '',  // Default is true
+    'youtube-allow-full-screen' => '',  // Default is true
+
+    // extras
+    'log'                 => '',  // Default is true
+
+  ), $atts);
+
+  $vjs_id       = $vjs_settings['id'];
+  $vjs_fix      = '';
+  $vjs_fix_dir  = '';
+  if ( $vjs_opts['slides'] ) {
+    $vjs_opts['slides'] = '> ' . $vjs_opts['slides'];
+  //   $vjs_id = str_replace('"', '', $vjs_id);
+  }
+  // echo $vjs_settings['css'] == 'true';
+
+  // If user wants default navs, add in the defaults
+  if ( $vjs_settings['navs'] ) {
+    if ( empty( $vjs_opts['prev'] ) ) {
+      $vjs_opts['prev'] = '.cycle-prev';
+    }
+    if ( empty( $vjs_opts['next'] ) ) {
+      $vjs_opts['next'] = '.cycle-next';
+    }
+  }
+
+
+  $vjs_opts_filter  = vjs_get_camel_case_opts_list($vjs_opts);
+
+  $vjs_div = '<div id="'. $vjs_settings['id'] .'" class="vjs-slider'. $vjs_settings['class'] .'"';
+  if ( $vjs_settings['width'] ) {
+    $vjs_div .= ' style="width:'. $vjs_settings['width'] .';"';
+  }
+  $vjs_div .= '>';
+
+  // Setup the nav links
+  $vjs_navLinks = vjs_nav_setup( $vjs_settings, $vjs_opts );
+
+  // Add in the nav links 
+  if ( 'inside' == $vjs_settings['navs'] ) {
+    $vjs_div .= do_shortcode( $content ) . $vjs_navLinks . '</div>';
+  } elseif ( 'outside' == $vjs_settings['navs'] ) {
+    $vjs_div .= do_shortcode( $content ) . '</div>' . $vjs_navLinks;
+  } else {
+    $vjs_div .= do_shortcode( $content ) . '</div>';
+  }
+
+  // Register the CSS and JavaScript only when the plugin is used
+  vjs_register_scripts( $vjs_settings, $vjs_opts, $vjs_fix_dir, $vjs_fix );
+
+  // vjs_slider_cycle_check();
+  vjs_slider_init_script( $vjs_opts_filter, $vjs_id, $vjs_settings );
+    
+  return $vjs_div;
+}
+
+add_shortcode( 'vjs_slider', 'vjs_slider' );
+
+
+
+
+function vjs_get_camel_case_opts_list( $vjs_opts ) {
+  $vjs_camel_case_list = '';
+  foreach (array_filter($vjs_opts) as $key => $value) {
+    
+    $key = str_replace(' ', '', ucwords(str_replace('-', ' ', $key)));
+    $key = lcfirst($key);
+
+    $vjs_camel_case_list .= "$key:";
+
+    if ( is_numeric($value) ) {
+      $vjs_camel_case_list .= "$value,";
+    } else {
+      if ($value == 'true') {
+        $vjs_camel_case_list .= "true,";
+      } elseif ($value == 'false') {
+        $vjs_camel_case_list .= "false,";
+      } elseif ($value == 'div' || $value == 'span' || $value == 'a') {
+        $vjs_camel_case_list .= "'> $value',";
+      } 
+      elseif ( strpos($value, '{') !== false ) {
+        $vjs_camel_case_list .= "$value,";
+      } 
+      else {
+        $vjs_camel_case_list .= "'$value',";
+      }
+    }
+  }
+  return $vjs_camel_case_list;
+}
+
+
+
+function vjs_nav_setup( $vjs_settings, $vjs_opts ) {
+
+  $vjs_div = '';
+
+  if ( $vjs_settings['navs'] ) {
+    // Add div if user provided 'nav-selector'
+    if ( ! empty( $vjs_settings['nav-selector'] ) ) {
+      if ( strpos( $vjs_settings['nav-selector'], '.' ) !== false ) {
+        $navSelector = 'class="'. str_replace( '.', '', $vjs_settings['nav-selector'] ) .'"';
+      } else {
+        $navSelector = 'id="'. str_replace( '#', '', $vjs_settings['nav-selector'] ) .'"';
+      }
+      $vjs_div .= '<div '. $navSelector .' style="visibility: visible;">';
+    }
+    
+
+    if ( ! empty( $vjs_opts['prev'] ) ) {
+      // Add the user provided class for the 'prev' link
+      if ( strpos( $vjs_opts['prev'], '.' ) !== false ) {
+        $prevSelector = 'class="'. str_replace( '.', '', $vjs_opts['prev'] ) .'"';
+      } else {
+        $prevSelector = 'id="'. str_replace( '#', '', $vjs_opts['prev'] ) .'"';
+      }
+    } else {
+      $prevSelector = 'class="cycle-prev"';
+    }
+    $vjs_div .= '<span '. $prevSelector .'>' . $vjs_settings['nav-prev'] . '</span>';
+
+    
+    if ( ! empty( $vjs_opts['prev'] ) ) {
+      // Add the user provided class for the 'next' link
+      if ( strpos( $vjs_opts['next'], '.' ) !== false ) {
+        $nextSelector = 'class="'. str_replace( '.', '', $vjs_opts['next'] ) .'"';
+      } else {
+        $nextSelector = 'id="'. str_replace( '#', '', $vjs_opts['next'] ) .'"';
+      }
+    } else {
+      $nextSelector = 'class="cycle-next"';
+    }
+    $vjs_div .= '<span '. $nextSelector .'>' . $vjs_settings['nav-next'] . '</span>';
+
+    // close up <div> if 
+    if ( ! empty( $vjs_settings['nav-selector'] ) ) {
+      $vjs_div .= '</div>';
+    }
+  }
+
+  return $vjs_div;
+}
+
+
+function vjs_slider_init_script( $vjs_opts_filter, $vjs_id, $vjs_settings ) {
+  $vjs_fix = '';
+  if ($vjs_settings['theme-fix']) {
+    $vjs_fix = '2';
+  }
+  // $vjs_slider_script = '';
+  // initialize the vjs_slider for this specific slider
+  $vjs_slider_script = 'jQuery(function($){';
+  $vjs_slider_script .= 'jQuery("#' . $vjs_id . '").cycle' . $vjs_fix . '({ '. $vjs_opts_filter . ' });';
+  $vjs_slider_script .= '});';
+
+  wp_add_inline_script( 'vjs.cycle2', $vjs_slider_script, 'before' );
+}
+
+// function vjs_slider_cycle_check() {
+//   $vjs_slider_cycle_check = '';
+//   // $vjs_slider_cycle_check = 'jQuery(function($){';
+//   // check if theme has already loaded the jquery.cycle plugin and log message
+//   // $vjs_slider_cycle_check .= 'setTimeout(function() {';
+//   $vjs_slider_cycle_check .= 'if(jQuery().cycle) {';
+//   $vjs_slider_cycle_check .= 'console.log("[vjs_slider] *** jquery.cycle plugin already loaded by theme! ***");';
+//   $vjs_slider_cycle_check .= 'console.log("[vjs_slider] *** Use the | theme-fix | attribute of the Versatile jQuery Slider plugin ***");';
+//   $vjs_slider_cycle_check .= '}';
+//   // $vjs_slider_cycle_check .= '}}, 100);';
+
+//   wp_add_inline_script( 'vjs.cycle2', $vjs_slider_cycle_check, 'before' );
+
+// }
+
+function vjs_register_scripts( $vjs_settings, $vjs_opts, $vjs_fix_dir, $vjs_fix ) {
+
+  if ($vjs_settings['theme-fix']) {
+    $vjs_fix = '.vjs-mod';
+    $vjs_fix_dir = '-vjs';
+  }
+  /*
+  * Register some very basic CSS
+  */
+  if ( 'true' == $vjs_settings['css'] ) { wp_enqueue_style( 'vjs-css',  plugins_url( '/css/vjs-slider.css', __FILE__ ) ); }
+
+  /*
+   * Register the jquery.cycle2 script
+   */
+  wp_enqueue_script('vjs.cycle2',  plugins_url( '/js/cycle2' . $vjs_fix_dir . '/jquery.cycle2' . $vjs_fix . '.min.js', __FILE__ ), 'jQuery', null, true );
+  
+  /*
+   * Conditionally register the plugin scripts
+   */
+  // transition scripts
+  if ( $vjs_opts['fx'] == 'carousel' ) { 
+    wp_enqueue_script('vjs.cycle2.carousel',  plugins_url( '/js/cycle2' . $vjs_fix_dir . '/jquery.cycle2' . $vjs_fix . '.carousel.min.js', __FILE__ ), 'cycle2', null, true );
+  } elseif ( $vjs_opts['fx'] == 'flipHorz' ) { 
+    wp_enqueue_script('vjs.cycle2.flip',  plugins_url( '/js/cycle2' . $vjs_fix_dir . '/jquery.cycle2' . $vjs_fix . '.flip.min.js', __FILE__ ), 'cycle2', null, true );
+  } elseif ( $vjs_opts['fx'] == 'scrollVert' ) { 
+    wp_enqueue_script('vjs.cycle2.scrollVert',  plugins_url( '/js/cycle2' . $vjs_fix_dir . '/jquery.cycle2' . $vjs_fix . '.scrollVert.min.js', __FILE__ ), 'cycle2', null, true );
+  } elseif ( $vjs_opts['fx'] == 'shuffle' ) { 
+    wp_enqueue_script('vjs.cycle2.shuffle',  plugins_url( '/js/cycle2' . $vjs_fix_dir . '/jquery.cycle2' . $vjs_fix . '.shuffle.min.js', __FILE__ ), 'cycle2', null, true );
+  } elseif ( $vjs_opts['fx'] == 'tileSlide' || $vjs_opts['fx'] == 'tileBlind' ) { 
+    wp_enqueue_script('vjs.cycle2.tileSlide',  plugins_url( '/js/cycle2' . $vjs_fix_dir . '/jquery.cycle2' . $vjs_fix . '.tile.min.js', __FILE__ ), 'cycle2', null, true );
+  }
+  
+  // swipe script
+  if ( 'true' == $vjs_opts['swipe'] ) { wp_enqueue_script('vjs.cycle2.swipe',  plugins_url( '/js/cycle2' . $vjs_fix_dir . '/jquery.cycle2' . $vjs_fix . '.swipe.min.js', __FILE__ ), 'cycle2', null, true ); }
+  
+  // centering script
+  if ( $vjs_opts['center-vert'] || $vjs_opts['center-horz'] ) { wp_enqueue_script( 'vjs.cycle2.center',  plugins_url( '/js/cycle2' . $vjs_fix_dir . '/jquery.cycle2' . $vjs_fix . '.center.min.js', __FILE__ ), 'cycle2', null, true ); }
+
+  // youtube video script
+  if ( $vjs_opts['youtube'] ) { wp_enqueue_script( 'vjs.cycle2.youtube',  plugins_url( '/js/cycle2' . $vjs_fix_dir . '/jquery.cycle2' . $vjs_fix . '.video.min.js', __FILE__ ), 'cycle2', null, true ); }
+
+  // ie fade script
+  if ( $vjs_settings['ie-fade'] ) { wp_enqueue_script( 'vjs.cycle2.ie-fade',  plugins_url( '/js/cycle2' . $vjs_fix_dir . '/jquery.cycle2' . $vjs_fix . '.ie-fade.min.js', __FILE__ ), 'cycle2', null, true ); }
+}
+
+?>
+
+<?php 
+/*
+
+Variations:
+
+slides: don't add the '>', it will automatically be assumed
+navs: If 'outside' and you're using the default CSS, you'll need to add your own CSS to make things look right
+next and prev selectors: Must add own CSS.  Note: if the navs are inside the slider container they will be hidden by default
+slide-css: not used because it didn't work reliably. If you want slide specific CSS, just add your own class to your slide and add your own CSS
+swipe: automatically added, because let's face it, who doesn't want that
+
+*/
+?>
+
